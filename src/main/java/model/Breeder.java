24bcbb9d633400ef991154efc66c30e2840892d2
Sankar Parameswaran <sankarpa@thoughtworks.com>
2017-12-26 16:38:10 +0530

    Remove unused methods and tests

--- src/main/java/model/Breeder.java
+++ src/main/java/model/Breeder.java
@@ -14,27 +14,6 @@ class Breeder {
         this.pokeBag = new PokeBag();
     }
 
-    /**
-     * @param pokeString
-     * @return
-     * @throws InvalidPokeStringException
-     */
-    public boolean addToBag(String pokeString) throws InvalidPokeStringException {
-
-        try {
-            if (StringUtils.isBlank(pokeString) || !pokeString.contains("#")) {
-                throw new InvalidPokeStringException("String is Empty or Not contains delimiter #");
-            }
-            String[] pokeMonParams = pokeString.split("#");
-            PokemonType pokemonType = PokemonType.valueOf(pokeMonParams[0].toUpperCase());
-            Integer level = new Integer(pokeMonParams[1]);
-            Pokemon pokemon = new Pokemon(pokemonType, level);
-            return pokeBag.add(pokemon);
-        } catch (Exception e) {
-            throw new InvalidPokeStringException(e.getMessage());
-        }
-    }
-
     public void arrangePokeMons(PokeBag opponetPokeBag) {
 
         PokeBagSorter sorter = new PokeBagSorter();
